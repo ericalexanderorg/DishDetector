3a3d49d830e5cf08b1f1ea2d308d6c234ab58a8b
Eric Alexander <eric.alexander@thetradedesk.com>
2018-01-28 10:02:28 -0700

    update setup script

--- dishdetector_setup.sh
+++ dishdetector_setup.sh
@@ -18,11 +18,11 @@ apt -y install python-opencv numpy apache2 python-pip
 pip install slackclient
 
 # Create our image output directory for troubleshooting purposes
-mkdir /var/www/html
+mkdir /var/www/html/images
 
 # Create a home for our python script and copy it over
 mkdir /opt/dishdetector
 cp dishdetector.py /opt/dishdetector/
 
-# Create a cronjob for our dishdetector
-crontab -l | { cat; echo "*/10 9-20 * * * SLACK_BOT_TOKEN="replace_with_your_slack_token" python /opt/dishdetector/dishdetector.py"; } | crontab -
+# Create a cronjob for our dishdetector that runs every 10 minutes from 9am to 10pm
+crontab -l | { cat; echo "*/10 9-22 * * * SLACK_BOT_TOKEN="replace_with_your_slack_token" python /opt/dishdetector/dishdetector.py"; } | crontab -
